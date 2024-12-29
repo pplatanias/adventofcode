08acde9ef79e4548e16fa77a41aba1e3718f4e6e
pplatanias <4315988+pplatanias@users.noreply.github.com>
2024-12-29 18:16:00 +0100

    Add 2015/18

---
+++ src/2015/day18.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <filesystem>
+#include <algorithm>
+
+int find_neighbors(std::vector<std::vector<char>> &matrix, int y, int x)
+{
+    std::vector<std::pair<int, int>> directions = {
+        {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    int alive_neighbors = 0;
+    int yy, xx;
+    for (std::pair dir : directions)
+    {
+        yy = y + dir.first, xx = x + dir.second;
+        if (yy >= 0 && yy < matrix.size() && xx >= 0 && xx < matrix[yy].size())
+        {
+            if (matrix[yy][xx] == '#')
+            {
+                alive_neighbors += 1;
+            }
+        }
+    }
+    return alive_neighbors;
+}
+
+int solve(bool part2 = false)
+{
+    std::ifstream file("inputs/2015/day18.txt");
+    if (!file)
+    {
+        std::cerr << "Unable to open file\n";
+        return 1;
+    }
+
+    std::vector<std::vector<char>> matrix; // 2D vector to store characters
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::vector<char> row(line.begin(), line.end()); // Convert string to vector of chars
+        matrix.push_back(row);
+    }
+
+    std::vector<std::vector<char>> matrix2(100, std::vector<char>(100, '.'));
+
+    for (int i = 0; i < 100; i++)
+    {
+        for (size_t y = 0; y < matrix.size(); y++)
+        {
+            for (size_t x = 0; x < matrix[y].size(); x++)
+            {
+                int neighbors = find_neighbors(matrix, y, x);
+                if (matrix[y][x] == '#')
+                {
+                    if (neighbors == 2 || neighbors == 3)
+                    {
+                        matrix2[y][x] = '#';
+                    }
+                    else
+                    {
+                        matrix2[y][x] = '.';
+                    }
+                }
+                else
+                {
+                    if (neighbors == 3)
+                    {
+                        matrix2[y][x] = '#';
+                    }
+                    else
+                    {
+                        matrix2[y][x] = '.';
+                    }
+                }
+            }
+        }
+        // Correct for Part 2
+        if (part2)
+        {
+            matrix2[0][0] = '#';
+            matrix2[99][0] = '#';
+            matrix2[0][99] = '#';
+            matrix2[99][99] = '#';
+        }
+        std::swap(matrix, matrix2);
+    }
+
+    int count = 0;
+    for (const auto &row : matrix)
+    {
+        count += std::count(row.begin(), row.end(), '#');
+    }
+    return count;
+}
+
+int main()
+{
+    std::cout << solve() << std::endl;
+    std::cout << solve(true) << std::endl;
+}
