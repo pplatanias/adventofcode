8cc2ebb51cf62d0a3606d7393c30cae5641ff964
pplatanias <4315988+pplatanias@users.noreply.github.com>
2025-01-03 17:30:02 +0100

    Add 2016 first four days

---
+++ src/2016/day03.py
@@ -0,0 +1,26 @@
+from itertools import batched
+
+with open('inputs/2016/day3.txt','r') as file:
+    matrix = [[int(y) for y in x.split()] for x in file.read().split('\n')]
+
+# Part 1
+impossible = 0
+for row in matrix:
+    if max(row) < (sum(row) - max(row)):
+        impossible += 1
+print(impossible)
+
+# Part 2
+rows = len(matrix)
+cols = len(matrix[0])
+transposed = [[0 for _ in range(rows)] for _ in range(cols)]
+for i in range(rows):
+    for j in range(cols):
+        transposed[j][i] = matrix[i][j]
+
+impossible = 0
+for row in transposed:
+    for triangle in batched(row, 3):
+        if max(triangle) < (sum(triangle) - max(triangle)):
+            impossible += 1
+print(impossible)
---
+++ src/2016/day04.py
@@ -0,0 +1,32 @@
+import re
+from itertools import groupby
+
+with open('inputs/2016/day4.txt','r') as file:
+    rows = re.findall(r'(.*-)(\d*)\[(.*)\]', file.read())
+
+# Part 1
+total = 0
+new_rows = []
+for room in rows:
+    group = sorted(sorted(room[0].replace('-','')), key=lambda x: room[0].count(x), reverse=True)
+    crc = ''.join(list({x:None for x in group})[0:5])
+    if crc == room[2]:
+        total += int(room[1])
+        new_rows.append(room)
+print(total)
+
+# Part 2
+decrypted = []
+for room in new_rows:
+    new_name = ''
+    offset = int(room[1])
+    for ch in room[0]:
+        if ch == '-':
+            new_name+=ch
+        else:
+            start = offset - (26 - (ord(ch) - ord('a')))
+            modded = start % 26
+            new_name += chr(ord('a') + modded)
+    decrypted.append((new_name,room[1]))
+
+[print(x) for x in decrypted if 'north' in x[0]]
